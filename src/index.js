fdde72d2104178a5ee018444d53dea1cb9a8a31d
AndrewSparrow <8911102@gmail.com>
2024-02-24 21:16:54 +0600

    adds normalize to project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
+import { Normalize } from 'styled-normalize'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>
 );
