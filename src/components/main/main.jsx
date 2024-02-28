bad0499f4f06bffafcf7f25740490807661f33ce
AndrewSparrow <8911102@gmail.com>
2024-02-28 20:58:31 +0600

    adds author link to the top of page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,6 +5,7 @@ import {PaginatedItems} from '../paginated-items/paginated-items';
 const Main = () => {
   return (
     <div className={styles.app}>
+      <a className={styles.author} href="https://github.com/Andrew-Sparrow" target='_blank' rel='noreferrer'>Produced by Andrew Pechersky</a>
       <h1 className={styles.title}>Products</h1>
       <main className={styles.main}>
         <PaginatedItems />
