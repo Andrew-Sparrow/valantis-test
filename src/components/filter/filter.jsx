055a76fe54b0517245ed2cd513abb5ae488a5adf
AndrewSparrow <8911102@gmail.com>
2024-02-29 12:30:05 +0600

    adds conditinal rendering to filter component

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
+import {getIsAllProductIDsLoading, getIsInitialItemsLoading} from '../../store/products/selectors';
 import styles from './filter.module.scss';
 
 
 const Filter = () => {
+  const isAllProductIDsLoading = useSelector(getIsAllProductIDsLoading);
+  const isInitialItemsLoading = useSelector(getIsInitialItemsLoading);
+
+  if (isAllProductIDsLoading || isInitialItemsLoading) {
+    return ''
+  }
+
   return (
     <div className={styles.filter}>
       <section className={styles.row}>
@@ -12,15 +21,13 @@ const Filter = () => {
           <option value="price">Price</option>
           <option value="brand">Brand</option>
         </select>
-        <input type="text" className={styles.input}/>
+        <input type="text" className={styles.input} />
       </section>
       <section>
         <button className={styles.button}>Submit filter</button>
-        <button className={`${styles.button} ${styles.button_reset}`}>Reset filter</button>
+        <button className={`${ styles.button } ${ styles.button_reset }`}>Reset filter</button>
       </section>
     </div>
-
-
   )
 };
 
