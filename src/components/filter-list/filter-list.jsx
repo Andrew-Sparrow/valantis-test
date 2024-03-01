8f475c1cc06add57dd297071e7623cae5509a9c3
AndrewSparrow <8911102@gmail.com>
2024-03-01 19:26:41 +0600

    adds basic components for filter pagination

---
+++ src/components/filter-list/filter-list.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProductList } from '../product-list/product-list';
+
+const FilterItems = () => {
+  return (
+    <div>FilterList</div>
+  )
+};
+
+export {FilterItems}
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,14 @@
 import styles from './main.module.scss';
 import {PaginatedItems} from '../paginated-items/paginated-items';
 import {Filter} from '../filter/filter';
+import {getIsFilterItemsDisplayed} from '../../store/products/selectors';
+import {FilterItems} from '../filter-list/filter-list';
+import {useSelector} from 'react-redux';
 
 
 const Main = () => {
+  const isFilterItemsDisplayed = useSelector(getIsFilterItemsDisplayed);
+
   return (
     <div className={styles.container}>
       <div className={styles.app}>
@@ -16,7 +21,7 @@ const Main = () => {
         <h1 className={styles.title}>Products</h1>
         <Filter />
         <main className={styles.main}>
-          <PaginatedItems />
+          {isFilterItemsDisplayed ? <FilterItems/> : <PaginatedItems />}
         </main>
       </div>
     </div>
