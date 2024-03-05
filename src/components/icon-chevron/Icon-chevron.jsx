cca574e1b3ff8fc7a09296a44c7d7365d776e592
AndrewSparrow <8911102@gmail.com>
2024-03-05 12:51:28 +0500

    changes the path icon -v3

--- src/components/icon-chevron/Icon-chevron.jsx
+++ src/components/icon-chevron/Icon-chevron.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export const IconChevronLeft = () => (
   <img
     className="prev-icon"
-    src={process.env.PUBLIC_URL + './assets/img/icons/Chevron_Left.svg'}
+    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Left.svg'}
     width="24"
     height="24"
     alt="chevron left"
@@ -16,7 +16,7 @@ export const IconChevronLeft = () => (
 export const IconChevronRight = () => (
   <img
     className="next-icon"
-    src={process.env.PUBLIC_URL + './assets/img/icons/Chevron_Right.svg'}
+    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Right.svg'}
     width="24"
     height="24"
     alt="chevron right"
