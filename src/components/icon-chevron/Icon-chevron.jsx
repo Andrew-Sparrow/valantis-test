276a8af400c6c72217daa3f1ce6f9b5f7ce335c5
AndrewSparrow <8911102@gmail.com>
2024-03-05 11:41:09 +0500

    change the path to the public folder for svg icons

--- src/components/icon-chevron/Icon-chevron.jsx
+++ src/components/icon-chevron/Icon-chevron.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { HandySvg } from 'handy-svg';
-import ChevronLeft from './../../img/icons/Chevron_Left.svg';
-import ChevronRight from './../../img/icons/Chevron_Right.svg';
+// import ChevronLeft from './../../img/icons/Chevron_Left.svg';
+// import ChevronRight from './../../img/icons/Chevron_Right.svg';
 
 export const IconChevronLeft = () => (
-  <HandySvg
+  <img
     className="prev-icon"
-    src={ChevronLeft}
+    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Left.svg'}
     width="24"
     height="24"
+    alt="chevron left"
   />
 );
 
 export const IconChevronRight = () => (
   <HandySvg
     className="next-icon"
-    src={ChevronRight}
+    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Right.svg'}
     width="24"
     height="24"
+    alt="chevron right"
   />
 );
