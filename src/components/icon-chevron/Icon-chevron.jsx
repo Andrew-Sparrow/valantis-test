b99afb594f09e7578ae2957fe3ff91beebb297c6
AndrewSparrow <8911102@gmail.com>
2024-03-05 12:16:23 +0500

    [fix] img svg

--- src/components/icon-chevron/Icon-chevron.jsx
+++ src/components/icon-chevron/Icon-chevron.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HandySvg } from 'handy-svg';
+// import { HandySvg } from 'handy-svg';
 // import ChevronLeft from './../../img/icons/Chevron_Left.svg';
 // import ChevronRight from './../../img/icons/Chevron_Right.svg';
 
 export const IconChevronLeft = () => (
   <img
     className="prev-icon"
-    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Left.svg'}
+    src={process.env.PUBLIC_URL + './assets/img/icons/Chevron_Left.svg'}
     width="24"
     height="24"
     alt="chevron left"
@@ -14,9 +14,9 @@ export const IconChevronLeft = () => (
 );
 
 export const IconChevronRight = () => (
-  <HandySvg
+  <img
     className="next-icon"
-    src={process.env.PUBLIC_URL + 'assets/img/icons/Chevron_Right.svg'}
+    src={process.env.PUBLIC_URL + './assets/img/icons/Chevron_Right.svg'}
     width="24"
     height="24"
     alt="chevron right"
